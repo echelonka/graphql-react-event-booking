53884988d44108a13bd1be5fec793c69aef23891
marinaskripnik97@gmail.com <marinaskripnik97@gmail.com>
2019-08-23 15:26:41 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { json } from 'body-parser'
+import graphQlHttp from 'express-graphql'
+import mongoose from 'mongoose'
+
+import graphQlSchema from './graphql/schema'
+import graphQlResolvers from './graphql/resolvers'
+
+const app = express()
+
+app.use(json())
+
+app.use('/api', graphQlHttp({
+  schema: graphQlSchema,
+  rootValue: graphQlResolvers,
+  graphiql: true
+}))
+
+const mongoURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-h6otv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+
+mongoose.connect(mongoURL, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch(err => console.error(err))
---
+++ models/user.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdEvents: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Event'
+  }]
+})
+
+module.exports = model('User', userSchema)
